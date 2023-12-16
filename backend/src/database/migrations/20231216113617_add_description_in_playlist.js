a07ca4bb0aa4f61db426ee10f1866721ed6b0f08
carlosE1602 <carlosedu1602@gmail.com>
2023-12-16 08:40:55 -0300

    fix - add description column in playlist table

---
+++ backend/src/database/migrations/20231216113617_add_description_in_playlist.js
@@ -0,0 +1,20 @@
+/**
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('playlist', function(table) {
+    table.string('descricao', 100).nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('playlist', function(table) {
+    table.dropColumn('descricao');
+  });
+};
